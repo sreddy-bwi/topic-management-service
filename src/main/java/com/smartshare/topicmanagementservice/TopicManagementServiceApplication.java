8570d3b4304b7fb247b87ef5495158bd6095c171
Sethuram Ramalinga Reddy <ramalins@tcd.ie>
2022-05-04 22:49:48 +0100

    SMART_SHARE: Initial Commit

---
+++ src/main/java/com/smartshare/topicmanagementservice/TopicManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.smartshare.topicmanagementservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopicManagementServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run( TopicManagementServiceApplication.class, args );
+    }
+
+}
