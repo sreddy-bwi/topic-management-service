379684a8b8cc72fd9190f0bedd4c7aeada99e3f5
Sethuram Ramalinga Reddy <ramalins@tcd.ie>
2022-05-06 06:08:40 +0100

    SMART-SHARE: Initial Structure

--- topic-management-app/src/main/java/com/smartshare/topic_management/TopicManagementApplication.java
+++ topic-management-app/src/main/java/com/smartshare/topic_management/TopicManagementApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TopicManagementServiceApplication {
+public class TopicManagementApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run( TopicManagementServiceApplication.class, args );
+        SpringApplication.run( TopicManagementApplication.class, args );
     }
 
 }
